3577696063ef95b9a48a7e8ee3fbce697287c5b5
ahmadfathy97 <af1381@fayoum.edu.eg>
2020-05-05 05:02:32 +0200

    :)

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-  database: 'mongodb://127.0.0.1:27017/simple-api',
+  database: 'mongodb://hi:please do not play with this1@ds247430.mlab.com:47430/tiny-blog',
   secret: 'sd5sfds5fg5g5d54646SDss'
 }
