882accad4cd58fc3cfb5a13781edc7f579f32fd1
Ahmad Fathy <af1381@fayoum.edu.eg>
2020-05-08 21:48:00 +0200

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
-  // database: 'mongodb://hi:please do not play with this1@ds247430.mlab.com:47430/tiny-blog',
-  database: 'mongodb://127.0.0.1:27017/simple-api',
+  database: 'mongodb://hi:please do not play with this1@ds247430.mlab.com:47430/tiny-blog',
+  // database: 'mongodb://127.0.0.1:27017/simple-api',
   secret: 'sd5sfds5fg5g5d54646SDss'
 }
