e1b168c1662826dc23afbfd1853f04ad01ea2066
ahmadfathy97 <af1381@fayoum.edu.eg>
2020-05-14 06:33:19 +0200

    nothing to say...

--- api/comments.js
+++ api/comments.js
@@ -9,7 +9,7 @@ const verify = require('../verifyToken');
 router.delete('/:comment_id', verify, (req, res)=>{
   Comments.findById(req.params.comment_id, (err, comment)=>{
     if(err) res.json({msg: err});
-    if(comment.user_id == req.session.user._id){
+    if(comment.user_id == req.user._id){
       Comments.remove({_id: req.params.comment_id}, (err)=>{
         if (err) res.json({msg: err});
         res.json({msg: 'comment deleted'});
