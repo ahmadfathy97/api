1e6e28f62d2c58a852c0a32262d7ca0965203593
ahmadfathy97 <af1381@fayoum.edu.eg>
2021-01-13 16:47:24 +0200

    added a state of the response success to the search controller

--- controllers/searchController.js
+++ controllers/searchController.js
@@ -7,18 +7,17 @@ controller.SearchForUsers = (req, res) => {
   Users.find({username: {$regex : '.*' + req.query.username + '.*', $options: 'i'}})
   .select({username: 1, _id: 1, pic: 1})
   .exec((err, users) => {
-    if(err) res.json({msg: err});
+    if(err) res.json({success: false, msg: 'something went wrong'});
     if(users.length){
       let newUsers = [];
       users.forEach((user)=>{
         let newUser = user.toJSON();
-        if(newUser.password) delete user.password;
         if(newUser.pic) newUser.pic = `http://${req.hostname}/${newUser.pic}`;
         newUsers.push(newUser);
       });
-      res.json({searchResult: newUsers});
+      res.json({success: true, searchResult: newUsers});
     } else {
-      res.json({noResults: 'no users matches'});
+      res.json({success: true, noResults: 'no users matches'});
     }
   });
 };
